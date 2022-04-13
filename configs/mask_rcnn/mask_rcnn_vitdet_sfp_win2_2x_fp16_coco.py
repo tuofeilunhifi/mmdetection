ec73793b4825c799df39e4627721faff42751e3c
yunji.cjy <yunji.cjy@alibaba-inc.com>
2022-04-13 10:15:48 +0800

    update

--- configs/mask_rcnn/mask_rcnn_vitdet_sfp_win2_2x_fp16_coco.py
+++ configs/mask_rcnn/mask_rcnn_vitdet_sfp_win2_2x_fp16_coco.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../_base_/models/mask_rcnn_vitdet_sfp_win2.py',
-    '../_base_/datasets/coco_instance_vitdet.py',
-    '../_base_/schedules/schedule_2x_fp16_vitdet.py', '../_base_/default_runtime.py'
+    '../_base_/datasets/coco_instance_vitdet_win2.py',
+    '../_base_/schedules/schedule_2x_fp16_vitdet_win2.py', '../_base_/default_runtime.py'
 ]
 
 checkpoint_config = dict(interval=6)
