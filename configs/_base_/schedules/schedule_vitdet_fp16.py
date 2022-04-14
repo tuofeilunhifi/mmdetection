5d57aac48a80a7498c772630266e9e28510c6402
yunji.cjy <yunji.cjy@alibaba-inc.com>
2022-04-14 12:13:22 +0800

    update

--- configs/_base_/schedules/schedule_vitdet_fp16.py
+++ configs/_base_/schedules/schedule_vitdet_fp16.py
@@ -1,15 +1,15 @@
 # optimizer
-paramwise_options={
+paramwise_cfg={
     'norm': dict(weight_decay=0.),
     'bias': dict(weight_decay=0.),
     'pos_embed': dict(weight_decay=0.),
     'cls_token': dict(weight_decay=0.)
 }
 optimizer = dict(type='AdamW', lr=1e-4, betas=(0.9, 0.999), weight_decay=0.1, 
-                    constructor='TransformerFinetuneConstructor',
-                    model_type='vit',
-                    layer_decay=0.70,
-                    paramwise_options=paramwise_options)
+                    # constructor='TransformerFinetuneConstructor',
+                    # model_type='vit',
+                    # layer_decay=0.70,
+                    paramwise_cfg=paramwise_cfg)
 
 cumulative_iters = 4
 optimizer_config = dict(grad_clip=None, cumulative_iters=cumulative_iters)
@@ -17,7 +17,7 @@ optimizer_config = dict(grad_clip=None, cumulative_iters=cumulative_iters)
 lr_config = dict(
     policy='step',
     warmup='linear',
-    warmup_iters=250 * cumulative_iters,
+    warmup_iters=1,
     warmup_ratio=0.001,
     step=[22, 24])
 runner = dict(type='EpochBasedRunner', max_epochs=25)
